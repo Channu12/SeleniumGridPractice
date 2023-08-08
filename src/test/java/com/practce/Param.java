61098aafa88daf302188a738a5cb4a651cfbc22b
Channu12 <channume008@gmail.com>
2023-08-08 17:23:20 +0530

    changes done

--- src/test/java/com/practce/Param.java
+++ src/test/java/com/practce/Param.java
@@ -1,24 +1,18 @@
 package com.practce;
 
-import java.util.Objects;
-
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class Param {
 	WebDriver driver = null;
-	@Parameters("BROWSER")
+
 	@Test
-	public void launchBrowser(String BROWSER) {
-		if(Objects.isNull(BROWSER))
-		{
-			BROWSER=System.getProperty("BROWSER","DEFAULT MESSAGE");
-		}
+	public void launchBrowser() {
+		String BROWSER = System.getProperty("BROWSER", "DEFAULT Value");
 		System.out.println(BROWSER);
 		if (BROWSER.equalsIgnoreCase("chrome")) {
 			WebDriverManager.chromedriver().setup();
